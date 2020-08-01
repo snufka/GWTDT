18bd6408d4fcfd6fd64fce2d6e363b363cc421f4
snufka <52801250+snufka@users.noreply.github.com>
2020-08-01 23:46:40 +0200

    generating books DB

---
+++ src/booksDB.js
@@ -0,0 +1,20 @@
+const booksData = [
+  {
+    title: "the girl with the dragon tattoo",
+    id: 1,
+    discription: "",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81UhKOBDrCL.jpg",
+  },
+  {
+    title: "the girl who played with fire",
+    id: 2,
+    discription: "",
+    img: "https://images-na.ssl-images-amazon.com/images/I/A12eoRmZtsL.jpg",
+  },
+  {
+    title: "the girl who kicked the honrnets nest",
+    id: 3,
+    discription: "",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81kP8NZxm+L.jpg",
+  },
+];
